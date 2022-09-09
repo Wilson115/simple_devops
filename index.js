46dc512e11263c7b4de5c6951dcea9421e956551
Wilson115 <102275472+Wilson115@users.noreply.github.com>
2022-09-09 20:47:34 +0800

    change in index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Simple DevOps')
+  res.send('change from Simple DevOps')
 })
 
 app.listen(process.env.PORT, ()=>{
